07e99d4c0e690ef1874211fa1ac56332fa0300b5
Anthony Petrides <anthony.petrides@thetrainline.com>
2021-08-31 10:12:58 +0100

    fixup

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { Story } from '@storybook/react';
-import Header, { HeaderProps } from './Header';
+import Header, { HeaderProps, ColourPalette } from './Header';
 
 export default {
   title: 'Components/Header',
@@ -11,7 +11,17 @@ export default {
 } as Meta;
 
 // Create a master template for mapping args to render the Header component
+// eslint-disable-next-line react/jsx-props-no-spreading
 const Template: Story<HeaderProps> = (args) => <Header {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = { headerText: "I'm a Material UI Header 🕹️" };
+export const Example1 = Template.bind({});
+Example1.args = { headerText: "I'm a Material UI Header 🕹️" };
+
+export const Example2 = Template.bind({});
+Example2.args = {
+  backgroundColor: ColourPalette.PastelBlue,
+  headerText: "I'm a Material UI Header 🕹️",
+  navLinkText1: 'Home',
+  navLinkText2: 'About Us',
+  navLinkText3: 'Contact Us',
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,19 @@ import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
-enum ColourPalette {
-  PastelBlueDark = '#123C69',
-  PastelCoralDark = '#AC3B61',
-  PastelGrayLight = '#BAB2B5'
+export enum ColourPalette {
+  PastelBlue = '#3792CB',
+  PastelCoral = '#AC3B61',
+  PastelGreen = '#95C9A9'
 }
 export interface HeaderProps {
+  /**
+   * Link to navigate to when clicking the main header icon
+   */
+  headerIconLink: string;
   /**
    * What background color to use
    */
@@ -26,14 +30,26 @@ export interface HeaderProps {
    * Nav Link 1 contents
    */
   navLinkText1?: string;
+   /**
+   * Nav Link 1 url
+   */
+  navLink1?: string;
   /**
    * Nav Link 2 contents
    */
   navLinkText2?: string;
+   /**
+   * Nav Link 2 url
+   */
+  navLink2?: string;
   /**
    * Nav Link 1 contents
    */
   navLinkText3?: string;
+   /**
+   * Nav Link 3 url
+   */
+  navLink3?: string;
 }
 
 const divStyle = {
@@ -42,17 +58,21 @@ const divStyle = {
 };
 
 const Header: FC<HeaderProps> = ({
-  backgroundColor = ColourPalette.PastelCoralDark,
+  backgroundColor = ColourPalette.PastelCoral,
+  headerIconLink,
   headerText,
   navLinkText1,
+  navLink1,
   navLinkText2,
+  navLink2,
   navLinkText3,
+  navLink3,
 }: HeaderProps) => (
   <div style={divStyle}>
-    <AppBar style={'backgroundColor' ? { backgroundColor } : {}}>
+    <AppBar style={backgroundColor ? { backgroundColor } : {}}>
       <ToolBar>
-        <IconButton>
-          <MenuIcon />
+        <IconButton href={headerIconLink}>
+          <BeachAccessIcon />
         </IconButton>
         <Typography variant="h6">
           {headerText}
@@ -60,16 +80,27 @@ const Header: FC<HeaderProps> = ({
         {navLinkText1 && (
         <Button
           size="small"
-          variant="outlined"
-          onClick={() => { alert('clicked'); }}
+          href={navLink1}
         >
           {navLinkText1}
         </Button>
         )}
         {navLinkText2 && (
-        <Button size="small" variant="outlined">{navLinkText2}</Button>)}
+        <Button
+          size="small"
+          href={navLink2}
+        >
+          {navLinkText2}
+        </Button>
+        )}
         {navLinkText3 && (
-        <Button size="small" variant="outlined">{navLinkText3}</Button>)}
+        <Button
+          size="small"
+          href={navLink3}
+        >
+          {navLinkText3}
+        </Button>
+        )}
       </ToolBar>
     </AppBar>
   </div>
