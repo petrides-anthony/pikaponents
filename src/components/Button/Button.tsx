bfd2b93469eb04e9923ce31976ab5a4f08ff033b
Anthony Petrides <anthony.petrides@thetrainline.com>
2021-09-02 15:44:37 +0100

    fix up

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,25 +3,15 @@ import React, { FC } from 'react';
 import './button.css';
 
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
+  /** Is this the principal call to action on the page? */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
+  /** What background color to use */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
+  /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
+  /** Button contents */
   label: string;
-  /**
-   * Optional click handler
-   */
+  /** Optional click handler */
   onClick?: (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => void;
